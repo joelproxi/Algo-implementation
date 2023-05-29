90d246e24f3060e0cba8ae23754392c9ed1e5ce6
proxidev <joeledmond95@gmail.com>
2023-05-29 17:42:09 +0100

    algo binary search python

---
+++ Binary search/python_imp.py
@@ -0,0 +1,38 @@
+
+table = [3, 5, 9, 29, 38, 40, 50, 58, 69, 77, 82, 83, 86, 90]
+
+
+def binary_search(table: list[int], nber: int) -> str:
+    min: int = 0
+    max: int = len(table) - 1
+    is_found = True
+    mid: int = ( min + max) // 2
+    
+    while is_found:
+        if min == max:
+            if nber != table[mid]:
+                return "Not found"
+        
+        elif nber == table[mid]:
+            return f" {nber} found in possition {mid}"
+        
+        elif table[mid] > nber:
+            max = mid - 1   
+            mid = (min + max) // 2
+            print(table[min:mid])
+            if nber == table[mid]:
+                return f" {nber} found in possition {mid}"
+            
+        elif  table[mid] < nber:
+            min = mid + 1
+            mid = (min + max) // 2
+            print(table[mid:max])
+            if nber == table[mid]:
+                return f" {nber} found in possition {mid}"
+            
+        else :
+           return nber 
+            
+
+
+print(binary_search(table, 5))
